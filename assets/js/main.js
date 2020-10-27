6d4b82f1e1651f9b1d8e0309df81c9d93e52570f
Jimengeo <55094496+Jimengeo@users.noreply.github.com>
2020-10-27 10:03:30 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//Variables
+
+let nav = document.getElementById('nav');
+let menu = document.getElementById('links');
+
+window.addEventListener('scroll', function(){
+	console.log(window.pageYOffset);
+});
