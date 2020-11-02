587dc104700582567c942854ce34188fc4d2d6a1
Jimengeo <55094496+Jimengeo@users.noreply.github.com>
2020-11-02 14:27:07 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,20 @@
 let nav = document.getElementById('nav');
 let menu = document.getElementById('links');
 
+function menus(){
+	let Actual_Scroll = window.pageYOffset;
+	
+	if(Actual_Scroll <= 529){
+		nav.classList.remove('nav2');
+		nav.className = ('nav1');
+		nav.style.transition = '1s'
+	}else{
+		nav.classList.remove('nav1');
+		nav.className = ('nav2');
+	}
+}
+
 window.addEventListener('scroll', function(){
 	console.log(window.pageYOffset);
+	menus();
 });
