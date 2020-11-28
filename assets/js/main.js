00d1413cac4cd72c0ad6295612a4ba9ceb81dd63
Jimengeo <55094496+Jimengeo@users.noreply.github.com>
2020-11-28 12:30:19 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,14 @@ function menus(){
 	if(Actual_Scroll <= 529){
 		nav.classList.remove('nav2');
 		nav.className = ('nav1');
-		nav.style.transition = '1s'
+		nav.style.transition = '1s';
 	}else{
 		nav.classList.remove('nav1');
 		nav.className = ('nav2');
+		nav.style.transition = '1s';
 	}
+	
+	
 }
 
 window.addEventListener('scroll', function(){
