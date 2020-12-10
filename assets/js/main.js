6a01b385d961bf1c9e8c60e10ba8ca50cf88bfb2
Jimengeo <55094496+Jimengeo@users.noreply.github.com>
2020-12-09 23:56:31 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,21 @@ window.addEventListener('scroll', function(){
 	console.log(window.pageYOffset);
 	menus();
 });
+
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
